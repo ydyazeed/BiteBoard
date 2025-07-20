99c36dc41bbc514a891088d423af0775e6849842
Yazeed Shamsudeen <yazeed9895@gmail.com>
2025-07-20 19:54:27 +0530

    Add build script for Render deployment

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py migrate 
